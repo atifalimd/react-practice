c74ee0b09f5c852a43caebfa6a94d580af21b387
New account <newaccount@New-MacBook-Pro.local>
2024-06-11 07:33:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 2, packed: true },
+];
+
+export default function App(){
+  return(
+    <div>
+      <Header/>
+      <ListItems/>
+      <PackingList/>
+      <Stats/>
     </div>
-  );
+  )
 }
 
-export default App;
+function Header(){
+  return(
+    <h1 className="header">🏝️ FAR AWAY 💼</h1>
+  )
+}
+
+function ListItems(){
+  const[description, setDescription] = useState('')
+  const[quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if(!description) return ;
+
+  }
+  return(
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for 😍 trip</h3>
+      <select value={quantity} onChange={(e)=>setQuantity(e.target.value)}>
+        {Array.from({length:20},(_,i)=>i+1).map
+        ((num)=>
+        (<option value={num} key={num}>{num}</option>))}
+      </select>
+      <input placeholder="item.." type="text" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+      <button className="button">Add</button>
+    </form>
+  )
+}
+
+function PackingList(){
+  return(
+    <div className="list">
+      <ul>
+      {initialItems.map(eachItem=>(
+        <Item eachItem={eachItem}/>
+      ))}
+      </ul>
+    </div>
+  )
+}
+
+function Item({eachItem}){
+  return(
+    <div className="list">
+      <li>
+        <input type="checkbox"/>
+        {eachItem.quantity} {eachItem.description} ❌
+      </li>
+    </div>
+  )
+}
+
+function Stats(){
+  return(
+    <footer className="stats">
+      <h3>💼 You have X items in your list</h3>
+    </footer>
+  )
+}
